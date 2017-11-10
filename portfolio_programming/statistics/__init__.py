dce4dda9d82a1878bc5cf2d5d9296e65a146c914
chenhh <chenhh@x220>
2017-11-11 02:58:27 +0800

    add risk_adjusted functions

---
+++ portfolio_programming/statistics/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v3
+"""
+
+import sys
+
+
+def main():
+    recs = iter(sys.stdin.readlines())
+
+
+if __name__ == '__main__':
+    main()
