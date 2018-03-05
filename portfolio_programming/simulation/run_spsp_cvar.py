7be51649e91aff9512bf4c2ec92199f1b1b94233
chenhh <chenhh@x220>
2018-03-06 00:38:29 +0800

    replace print function to logging module

--- portfolio_programming/simulation/run_spsp_cvar.py
+++ portfolio_programming/simulation/run_spsp_cvar.py
@@ -5,18 +5,20 @@ License: GPL v3
 """
 
 import json
-import numpy as np
-import pandas as pd
-import xarray as xr
 import logging
+
+import numpy as np
+import xarray as xr
 
 import portfolio_programming as pp
 from portfolio_programming.simulation.spsp_cvar import SPSP_CVaR
 
 
-def run_SPSP_CVaR(n_stock, rolling_window_size, alpha):
-    risky_roi_xarr = xr.open_dataarray(pp.TAIEX_2005_LARGESTED_MARKET_CAP_DATA_NC)
-    candidate_symbols = json.load(open(pp.TAIEX_2005_LARGEST4ED_MARKET_CAP_SYMBOL_JSON))[:n_stock]
+def run_compact_SPSP_CVaR(n_symbol, rolling_window_size, alpha):
+    risky_roi_xarr = xr.open_dataarray(
+        pp.TAIEX_2005_LARGESTED_MARKET_CAP_DATA_NC)
+    candidate_symbols = json.load(
+        open(pp.TAIEX_2005_LARGEST4ED_MARKET_CAP_SYMBOL_JSON))[:n_symbol]
 
     risky_rois = risky_roi_xarr.loc[pp.EXP_START_DATE:pp.EXP_END_DATE,
                  candidate_symbols, 'simple_roi']
@@ -25,14 +27,47 @@ def run_SPSP_CVaR(n_stock, rolling_window_size, alpha):
     n_exp_dates = len(exp_trans_dates)
     risk_free_rois = xr.DataArray(np.zeros(n_exp_dates),
                                   coords=(exp_trans_dates,))
-    initial_risk_wealth = xr.DataArray(np.zeros(n_stock),
+    initial_risk_wealth = xr.DataArray(np.zeros(n_symbol),
                                        dims=('symbol',),
                                        coords=(candidate_symbols,))
     initial_risk_free_wealth = 1e6
 
     instance = SPSP_CVaR(candidate_symbols,
                          "compact",
-                         n_stock,
+                         n_symbol,
+                         risky_rois,
+                         risk_free_rois,
+                         initial_risk_wealth,
+                         initial_risk_free_wealth,
+                         rolling_window_size=rolling_window_size,
+                         alpha=alpha,
+                         scenario_set_idx=1,
+                         print_interval=10
+                         )
+    instance.run()
+
+
+def run_general_SPSP_CVaR(max_portfolio_size, rolling_window_size, alpha):
+    risky_roi_xarr = xr.open_dataarray(
+        pp.TAIEX_2005_LARGESTED_MARKET_CAP_DATA_NC)
+    candidate_symbols = json.load(
+        open(pp.TAIEX_2005_LARGEST4ED_MARKET_CAP_SYMBOL_JSON))
+
+    risky_rois = risky_roi_xarr.loc[pp.EXP_START_DATE:pp.EXP_END_DATE,
+                 candidate_symbols, 'simple_roi']
+
+    exp_trans_dates = risky_rois.get_index('trans_date')
+    n_exp_dates = len(exp_trans_dates)
+    risk_free_rois = xr.DataArray(np.zeros(n_exp_dates),
+                                  coords=(exp_trans_dates,))
+    initial_risk_wealth = xr.DataArray(np.zeros(n_symbol),
+                                       dims=('symbol',),
+                                       coords=(candidate_symbols,))
+    initial_risk_free_wealth = 1e6
+
+    instance = SPSP_CVaR(candidate_symbols,
+                         "general",
+                         max_portfolio_size,
                          risky_rois,
                          risk_free_rois,
                          initial_risk_wealth,
@@ -50,4 +85,5 @@ if __name__ == '__main__':
                                '%(message)s',
                         datefmt='%Y%m%d-%H:%M:%S',
                         level=logging.INFO)
-    run_SPSP_CVaR(10, 90, 0.5)
+    run_compact_SPSP_CVaR(10, 90, 0.5)
+    run_general_SPSP_CVaR(5, 200, 0.5)
