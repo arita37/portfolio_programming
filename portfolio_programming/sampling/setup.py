0da8771c5784af5c82b3e26b6ec913b1e15377fb
chenhh <chenhh@x220>
2018-03-05 20:25:15 +0800

    replace print function to logging module

--- portfolio_programming/sampling/setup.py
+++ portfolio_programming/sampling/setup.py
@@ -39,6 +39,7 @@ extensions = [
               "moment_matching",
               ["moment_matching.pyx"],
               include_dirs = [np.get_include()],
+              extra_compile_args = ['--with-fpectl',],
     ),
 ]
 
