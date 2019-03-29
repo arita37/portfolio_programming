a4b8fce3c5599f9d16105adde253054cb4771b7a
chenhh <chenhh@x220>
2019-03-29 12:43:20 +0800

    add scrp

---
+++ sample/bcrp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+"""
+
+import sys
+
+
+def main():
+    recs = iter(sys.stdin.readlines())
+
+
+if __name__ == '__main__':
+    main()
