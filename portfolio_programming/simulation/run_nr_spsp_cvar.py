e9ccc00125255dcb73b2dad03e45584614149921
chenhh <chenhh@x220>
2019-04-11 01:01:32 +0800

    update nir-spsp module

--- portfolio_programming/simulation/run_nr_spsp_cvar.py
+++ portfolio_programming/simulation/run_nr_spsp_cvar.py
@@ -457,7 +457,7 @@ if __name__ == '__main__':
                         help="pre-generated scenario set index.")
     parser.add_argument("--stat", default=False, action='store_true',
                         help="NR_SPSP_cVaR experiment statistics")
-    parser.add_argument("-s", "--server", default=False, action='store_true',
+    parser.add_argument("--server", default=False, action='store_true',
                         help="parameter server mode")
     parser.add_argument("-c", "--client", default=False, action='store_true',
                         help="run NR_SPSP_CVaR client mode")
@@ -472,7 +472,7 @@ if __name__ == '__main__':
         print("run NR_SPSP_CVaR parameter server mode")
         print("exp_name: {},regret:{}".format(
             args.exp_name,  args.regret))
-        parameter_server(args.exp_name, args.exp_name,  args.regret)
+        parameter_server(args.exp_name,  args.regret)
         sys.exit()
     elif args.client:
         print("run NR_SPSP_CVaR client mode")
